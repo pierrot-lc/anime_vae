2e59b377791fdd703f44c97cfc0ba27b76add8fc
Pierre PEREIRA <pierre.pereira@grenoble-inp.com>
2021-10-13 00:28:18 -0400

    debug: pass model in eval mode to use itgit add VAE.py

--- VAE.py
+++ VAE.py
@@ -333,4 +333,5 @@ def load_model(config, path_model='vae.pt'):
         config['res_layers']
     )
     model.load_state_dict(torch.load(path_model, map_location='cpu'))
+    model.eval()
     return model
