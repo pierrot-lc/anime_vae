d092c932e412a36c06063f28620a81b750775b39
PierrotLC <pstmrtem@outlook.com>
2022-09-30 20:01:51 +0200

    documentation: VAE4

--- src/vae.py
+++ src/vae.py
@@ -1,9 +1,14 @@
+"""
+Inspired from https://debuggercafe.com/getting-started-with-variational-autoencoder-using-pytorch/.
+"""
 import torch
 import torch.nn as nn
 from einops.layers.torch import Rearrange
 
 
 class ResBlock(nn.Module):
+    """A simple residual block.
+    """
     def __init__(self, n_filters: int, kernel_size: int):
         super().__init__()
         self.conv_block = nn.Sequential(
@@ -20,10 +25,25 @@ class ResBlock(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Residual forward.
+        Does not modify the shape of the input.
+
+        Args
+        ----
+            x: Input tensor.
+                Shape of [batch_size, n_filters, width, height].
+
+        Returns
+        -------
+            y: Output tensor.
+                Shape of [batch_size, n_filters, width, height].
+        """
         return self.conv_block(x) + x
 
 
 class ReduceBlock(nn.Module):
+    """Block reducing or upscaling the dimensions of the input tensor.
+    """
     def __init__(
         self,
         n_filters_in: int,
@@ -46,10 +66,24 @@ class ReduceBlock(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Reduce or upscale the dimensions of the input tensor.
+
+        Args
+        ----
+            x: Input tensor.
+                Shape of [batch_size, n_filters_in, width, height].
+
+        Returns
+            y: Output tensor.
+                Shape of [batch_size, n_filters_out, width/2, height/2] if `upscale`.
+                Shape of [batch_size, n_filters_out, width*2, height*2] if not `upscale`.
+        """
         return self.conv_block(x)
 
 
 class CNNEncoder(nn.Module):
+    """Project the input image to an embedding space.
+    """
     def __init__(self, n_channels: int, n_filters: int, n_layers: int):
         super().__init__()
 
@@ -74,6 +108,18 @@ class CNNEncoder(nn.Module):
         ])
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Project the input tensor.
+
+        Args
+        ----
+            x: Input tensor.
+                Shape of [batch_size, n_channels, width, height].
+
+        Returns
+        -------
+            x: Projected tensor.
+                Shape of [batch_size, n_filters << n_layers, width >> n_layers, height >> n_layers].
+        """
         x = self.project_layer(x)
         for layer in self.layers:
             x = layer(x)
@@ -81,6 +127,8 @@ class CNNEncoder(nn.Module):
 
 
 class VAEEncoder(nn.Module):
+    """Project the input image to a parameterized latent space.
+    """
     def __init__(
         self,
         n_channels: int,
@@ -103,12 +151,29 @@ class VAEEncoder(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Project the input images to a parameterized latent space.
+
+        Args
+        ----
+            x: Input images.
+                Shape of [batch_size, n_channels, width, height].
+
+        Returns
+        -------
+            x: Parameterized latent images.
+                The parameters are the `mu` and `log_var` parameters of a gaussian
+                distribution. They are separated along the dimension 1.
+                Shape of [batch_size, 2, n_channels_latent, width >> n_layers, height >> n_layers].
+        """
         x = self.cnn_encoder(x)
         x = self.project_latent(x)
         return x
 
 
 class CNNDecoder(nn.Module):
+    """Project back the input tensor from the latent space to its original space.
+    The RGB channels are not recovered in this module.
+    """
     def __init__(self, n_channels_latent: int, n_filters: int, n_layers: int):
         super().__init__()
         n_filters_start = n_filters << n_layers
@@ -134,6 +199,18 @@ class CNNDecoder(nn.Module):
         ])
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Project back the input tensor to its original shape.
+
+        Args
+        ----
+            x: Input embedded tensor.
+                Shape of [batch_size, n_channels_latent, width, height].
+
+        Returns
+        -------
+            x: Input tensor projected to its original shape.
+                Shape of [batch_size, n_filters, width << n_layers, height << n_layers].
+        """
         x = self.project_layer(x)
         for layer in self.layers:
             x = layer(x)
@@ -141,6 +218,8 @@ class CNNDecoder(nn.Module):
 
 
 class VAEDecoder(nn.Module):
+    """Project back the input images to the RGB space.
+    """
     def __init__(
         self,
         n_channels: int,
@@ -159,12 +238,27 @@ class VAEDecoder(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Project back the input images to the RGB space.
+
+        Args
+        ----
+            x: Input embedded tensor.
+                Shape of [batch_size, n_channels_latent, width, height].
+
+        Returns
+        -------
+            x: Input tensor projected to its original shape.
+                Shape of [batch_size, n_channels, width << n_layers, height << n_layers].
+        """
         x = self.cnn_decoder(x)
         x = self.project_rgb(x)
         return x
 
 
 class VAE(nn.Module):
+    """Project the input image to a parameterized latent space
+    and project it back to its original RGB space.
+    """
     def __init__(
         self,
         n_channels: int,
@@ -173,6 +267,9 @@ class VAE(nn.Module):
         n_channels_latent: int,
     ):
         super().__init__()
+        self.n_filters = n_filters
+        self.n_layers = n_layers
+        self.n_channels_latent = n_channels_latent
 
         self.encoder = VAEEncoder(n_channels, n_filters, n_layers, n_channels_latent)
         self.decoder = VAEDecoder(n_channels, n_filters, n_layers, n_channels_latent)
@@ -182,6 +279,22 @@ class VAE(nn.Module):
         mu: torch.Tensor,
         log_var: torch.Tensor
     ) -> torch.Tensor:
+        """Reparameterization trick.
+        Sample from the gaussian distribution parameterized by `mu` and `log_var`.
+        To keep the gradient flowing, we only sample a random noise and combine the parameters
+        together to simulate a sampling.
+
+        Args
+        ----
+            mu: Mean of the gaussian.
+                Shape of [batch_size, n_channels_latent, width, height].
+            log_var: LogVar of the gaussian.
+                Shape of [batch_size, n_channels_latent, width, height].
+
+        Returns
+        -------
+            sample: Simulated sample from the gaussian distribution that keeps the gradient flowing.
+        """
         std = torch.exp(0.5*log_var)  # Standard deviation
         eps = torch.randn_like(std)  # Small noise to simulate the sampling
         sample = mu + (eps * std)  # Sampling as if coming from the input space
@@ -191,6 +304,23 @@ class VAE(nn.Module):
         self,
         x: torch.Tensor,
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Compute the latent parameters of the input images, and tries to recreate them
+        from their latent space.
+
+        Args
+        ----
+            x: Input images.
+                Shape of [batch_size, n_channels, width, height].
+
+        Returns
+        -------
+            x: Output images.
+                Shape of [batch_size, n_channels, width, height].
+            mu: Mean of the gaussian.
+                Shape of [batch_size, n_channels_latent, width, height].
+            log_var: LogVar of the gaussian.
+                Shape of [batch_size, n_channels_latent, width, height].
+        """
         x = self.encoder(x)
 
         # Get `mu` and `log_var` from the encoder's output
@@ -201,3 +331,24 @@ class VAE(nn.Module):
 
         x = self.decoder(z)
         return x, mu, log_var
+
+    @torch.no_grad()
+    def generate(self, batch_size: int, image_size: int) -> torch.Tensor:
+        """Generate a sample of images from the latent space.
+
+        Args
+        ----
+            batch_size: Number of images to create.
+            image_size: Width and height of the images.
+
+        Output
+            sample: Sampled images from the latent space.
+                Shape of [batch_size, n_channels, image_size, image_size].
+        """
+        latent_shape = (batch_size, self.n_channels_latent, image_size >> self.n_layers, image_size >> self.n_layers)
+        mu = torch.zeros(latent_shape)
+        log_var = torch.zeros(latent_shape)
+        z = self.reparameterize(mu, log_var)
+        sample = self.decoder(z)
+        sample = torch.sigmoid(sample)
+        return sample
